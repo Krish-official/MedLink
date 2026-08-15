9f0e44fd98e373ec26dc148b8bfb373be47732b9
Krish <krish.choudhary.cs@gmail.com>
2026-08-15 14:24:28 +0530

    Routes Updated

--- medlink_app/lib/core/router/routes.dart
+++ medlink_app/lib/core/router/routes.dart
@@ -13,29 +13,24 @@ class Routes {
   static const String patientMedicalRecords = '/patient/medical-records';
   static const String patientPrescriptions = '/patient/prescriptions';
   static const String patientProfile = '/patient/profile';
+  static const String patientEmergency = '/patient/emergency';
 
   // Doctor
   static const String doctorDashboard = '/doctor/dashboard';
   static const String doctorSchedule = '/doctor/schedule';
   static const String doctorPatients = '/doctor/patients';
+  static String doctorPatientProfile(String id) => '/doctor/patients/$id';
   static const String doctorPrescriptions = '/doctor/prescriptions';
   static const String doctorReports = '/doctor/reports';
 
   // Admin
   static const String adminDashboard = '/admin/dashboard';
+  static const String adminDoctors = '/admin/doctors';
+  static const String adminPatients = '/admin/patients';
+  static const String adminAppointments = '/admin/appointments';
+  static const String adminSettings = '/admin/settings';
 
   // Public
   static const String landing = '/';
   static const String doctorDirectory = '/doctors';
-
-    // Patient Book Appointment
-  static const String patientBookAppointmentSearch = '/patient/book-appointment';
-  static const String patientBookAppointmentSlots = '/patient/book-appointment/slots';
-  static const String patientBookAppointmentConfirm = '/patient/book-appointment/confirm';
-  static const String patientBookAppointmentSuccess = '/patient/book-appointment/success';
-
-  // Doctor
-
-  static String doctorPatientProfile(String id) => '/doctor/patients/$id';
-
 }
